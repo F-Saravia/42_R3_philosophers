a84a770a4b4ae3eacff8288cb022d0735b6adf85
f-saravia <saraviatoledo.facundo@gmail.com>
2026-09-19 18:47:39 +0200

    mini refactor: move parsing related functions to a parse_argv.c

---
+++ philo/parse_argv.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_argv.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fasaravi <fasaravi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/19 18:44:46 by fasaravi          #+#    #+#             */
+/*   Updated: 2026/09/19 18:46:22 by fasaravi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	remember_noet(int n_args)
+{
+	ft_puts_fd(STDERR_FILENO, "Expected number of arguments: 4[+1]");
+	ft_putstr_fd(STDERR_FILENO, "Received ");
+	ft_putnbr_fd(STDERR_FILENO, (long) n_args);
+	ft_puts_fd(STDERR_FILENO, " arguments");
+	ft_puts_fd(STDERR_FILENO,\
+"./philo nb_philos time_die time_eat time_sleep [nb_meals]\n");
+}
+
+static bool	valid_argv(int ac, t_state *state)
+{
+	bool	valid;
+
+	valid = true ;
+	if (state->nb_meals < 0)
+		valid = (false);
+	if (state->time_die < 0)
+		valid = (false);
+	if (state->time_eat < 0)
+		valid = (false);
+	if (state->time_sleep < 0)
+		valid = (false);
+	if (ac == 6 && state->nb_meals < 0)
+		valid = (false);
+	if (valid == false)
+		ft_puts_fd(STDERR_FILENO, "Expect args to be positive numbers");
+	return (valid);
+}
+
+bool	parse_argv(int ac, char **av, t_state *state)
+{
+	int	i;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (!ft_isnbr(av[i]))
+		{
+			ft_puts_fd(STDERR_FILENO, "Expect args to be positive numbers");
+			return (false);
+		}
+		i++;
+	}
+	state->nb_philo = ft_atoi(av[1]);
+	state->time_die = ft_atoi(av[2]);
+	state->time_eat = ft_atoi(av[3]);
+	state->time_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		state->nb_meals = ft_atoi(av[5]);
+	else
+		state->nb_meals = -1;
+	return (valid_argv(ac, state));
+}
